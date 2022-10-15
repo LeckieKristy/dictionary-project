9ba5c220d3501bd3ebf62c5dac2a4342fd3e69d4
LeckieKristy <100957443+LeckieKristy@users.noreply.github.com>
2022-10-15 11:01:42 -0600

    file setup/ bootstrap installed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          hello
+        </p>
+ 
+      </header>
+    </div>
+  );
+}
+
