66d347a9d535c74c15f3ebaf588d8aad1d9ab835
LeckieKristy <bkris030@gmail.com>
2022-12-20 14:22:00 -0600

    Moved files to new computer, fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
-import Dictionary from "./Dictionary"
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
-      <header className="App-header">
-        
-        <h1 className="text-center">
-       Dictionary App
-        </h1>
-       
- 
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer className='footer'>
-        Coded by Kristy Leckie and hosted on <a href="https://github.com/LeckieKristy/dictionary-project" target="_blank"> GitHub</a>
-     </footer>
-    </div>
+      <div className="container">
+        <header className="App-header">
+          <h1 className="text-center">Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer">
+          Coded by Kristy Leckie and hosted on{" "}
+          <a
+            href="https://github.com/LeckieKristy/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
